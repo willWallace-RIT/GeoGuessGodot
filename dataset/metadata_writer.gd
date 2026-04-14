dfccc81b6609e7c507902ca9cdbcbdcf85fcbff4
northbot <54906390+willWallace-RIT@users.noreply.github.com>
2026-04-14 12:30:24 -0400

    Update metadata_writer.gd

--- dataset/metadata_writer.gd
+++ dataset/metadata_writer.gd
@@ -1,11 +1,21 @@
+extends Node
+
+var region_mapper = preload("res://addons/geoguess_ai/utils/region_mapper.gd").new()
+
 func save_metadata(filename):
     var cam = get_viewport().get_camera_3d()
-    
+    if cam == null:
+        return
+
     var data = {
         "file": filename,
         "position": cam.global_position,
-        "region": get_region_from_position(cam.global_position)
+        "region": region_mapper.get_region(cam.global_position)
     }
-    
-    var file = FileAccess.open("user://dataset/labels.json", FileAccess.WRITE_READ)
-    file.store_line(JSON.stringify(data))
+
+    var path = "user://dataset/labels.jsonl"
+    var file = FileAccess.open(path, FileAccess.READ_WRITE)
+
+    if file:
+        file.seek_end()
+        file.store_line(JSON.stringify(data))
